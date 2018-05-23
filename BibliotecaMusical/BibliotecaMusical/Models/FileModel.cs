f9be6a36ba680a9aa1eee1ae66dea8cdab8faa01
Pablo Gonzalez <pablo.gonzalez@proximitycr.com>
2018-05-23 08:47:27 -0600

    Code cleanup.

--- BibliotecaMusical/BibliotecaMusical/Models/FileModel.cs
+++ BibliotecaMusical/BibliotecaMusical/Models/FileModel.cs
@@ -3,20 +3,6 @@
 namespace BibliotecaMusical.Models {
 	public class FileModel {
 		public Guid Id { get; set; }
-		//{
-		//	get {
-				//return Name
-				//	.Replace(" ", string.Empty)
-				//	.Replace("(", string.Empty)
-				//	.Replace(")", string.Empty)
-				//	.Replace("[", string.Empty)
-				//	.Replace("]", string.Empty)
-				//	.Replace("#", string.Empty)
-				//	.Replace("-", string.Empty)
-				//	.Replace("_", string.Empty)
-				//	.Replace(".mp3", string.Empty);
-		//	}
-		//}
 		public string Name { get; set; }
 		public string Size { get; set; }
 	}
